7ef99c29ef556535e141afe5f5898b9a39aa67c4
lcastillo2689 <88407795+lcastillo2689@users.noreply.github.com>
2022-03-07 15:37:55 -0600

    Add files via upload

---
+++ idev35i3.sql
@@ -0,0 +1,7 @@
+﻿insert into okr_cat_roles(rol_id, descripcion) values (4, 'Capacitador')
+/
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES (N'20220307194110_AddCapacitadorToRolesTable', N'5.0.12')
+/
+
